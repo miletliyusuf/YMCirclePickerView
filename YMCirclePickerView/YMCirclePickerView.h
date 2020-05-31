19fec1ceea0d605c6ce08a9db9637c3367790609
Yusuf Miletli <miletliyusuf@gmail.com>
2020-05-31 16:06:46 +0300

    Init project

---
+++ YMCirclePickerView/YMCirclePickerView.h
@@ -0,0 +1,19 @@
+//
+//  YMCirclePickerView.h
+//  YMCirclePickerView
+//
+//  Created by Yusuf Miletli on 31.05.2020.
+//  Copyright © 2020 Miletli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YMCirclePickerView.
+FOUNDATION_EXPORT double YMCirclePickerViewVersionNumber;
+
+//! Project version string for YMCirclePickerView.
+FOUNDATION_EXPORT const unsigned char YMCirclePickerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YMCirclePickerView/PublicHeader.h>
+
+
