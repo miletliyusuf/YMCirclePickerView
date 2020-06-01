49f6a49c19f4f94d705a08a903067b2b1654c8be
Yusuf Miletli <miletliyusuf@gmail.com>
2020-06-01 12:11:11 +0300

    Add styling for selection view

--- YMCirclePickerView/Views/YMCirclePickerView/YMCirclePickerView.swift
+++ YMCirclePickerView/Views/YMCirclePickerView/YMCirclePickerView.swift
@@ -9,11 +9,22 @@
 import Foundation
 import UIKit
 
+public struct YMCirclePickerViewStylePresentation {
+
+    /// Selection View
+    var selectionColor: UIColor = .white
+    var selectionLineWidth: CGFloat = 1.0
+
+    /// Title Label
+    var titleLabelDistance: CGFloat = 0.0
+}
+
 // MARK: - YMCirclePickerView
 
 public struct YMCirclePickerViewPresentation {
 
     public var layoutPresentation: YMCirclePickerViewLayoutPresentation
+    public var stylePresentation: YMCirclePickerViewStylePresentation
 }
 
 // MARK: - YMCirclePickerViewDelegate
@@ -43,7 +54,14 @@ public class YMCirclePickerView: UIView, NibLoadable {
     // MARK: - Outlets
 
     @IBOutlet private weak var collectionView: UICollectionView!
+    @IBOutlet private weak var selectionView: UIView!
     @IBOutlet private weak var titleLabel: UILabel!
+
+    // MARK: Constraints
+
+    @IBOutlet private weak var collectionTitleVerticalConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var selectionViewHeightContstraint: NSLayoutConstraint!
+    @IBOutlet private weak var selectionViewWidthContstraint: NSLayoutConstraint!
 
     // MARK: - Properties
 
@@ -83,6 +101,28 @@ public class YMCirclePickerView: UIView, NibLoadable {
         collectionView.collectionViewLayout = YMCirclePickerViewLayout(
             presentation: presentation.layoutPresentation
         )
+
+        // Selection view
+
+        selectionViewHeightContstraint.constant = presentation.layoutPresentation.itemSize.height + presentation.stylePresentation.selectionLineWidth
+        selectionViewWidthContstraint.constant = presentation.layoutPresentation.itemSize.width + presentation.stylePresentation.selectionLineWidth
+
+        layoutIfNeeded()
+
+        let circlePath = UIBezierPath(
+            arcCenter: selectionView.center,
+            radius: selectionView.frame.size.width / 2.0,
+            startAngle: CGFloat(0),
+            endAngle: CGFloat(Double.pi * 2),
+            clockwise: true
+        )
+
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = presentation.stylePresentation.selectionColor.cgColor
+        shapeLayer.lineWidth = presentation.stylePresentation.selectionLineWidth
+        selectionView.layer.addSublayer(shapeLayer)
     }
 }
 
